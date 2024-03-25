f6373fc1e810e37a2be6d6f1de55474c21b0c631
Ben Eastwood <83828090+k4ben@users.noreply.github.com>
2024-03-24 20:04:03 -0400

    latest

--- src/lib/CategoryDropdown.svelte
+++ src/lib/CategoryDropdown.svelte
@@ -1,44 +1,36 @@
 <script lang="ts">
 	import type { Category } from '@prisma/client';
 	import { convertToHierarchy2 } from './functions';
+	import { v4 } from 'uuid';
+	const groupId = v4().substring(0, 4);
 
 	export let name: string | undefined = undefined;
+	const root = name == undefined;
 	export let categories: ReturnType<typeof convertToHierarchy2>;
 </script>
 
-{#if name == undefined}
-	<div class="group/a inline-block relative">
-		<div
-			class="hidden group-hover/a:join join-vertical absolute top-full border border-base-200 outline-1 bg-base-100 divide-y divide-neutral"
-		>
-			{#each categories as c}
-				{#if c.children.length == 0}
-					<div class="join-item">{c.Name}</div>
-				{:else}
-					<svelte:self categories={c.children} name={c.Name} />
-				{/if}
-			{/each}
-		</div>
+<div class={`inline-block relative group/[${groupId}]`} class:join-item={!root}>
+	<div
+		class={`hidden group-hover/[${groupId}]:join join-vertical absolute border border-base-200 outline-1 bg-base-100 divide-y divide-neutral ${
+			root ? 'top-full' : 'left-full top-0'
+		}`}
+	>
+		{#each categories as c}
+			{#if c.children.length == 0}
+				<div class="join-item">{c.Name}</div>
+			{:else}
+				<svelte:self categories={c.children} name={c.Name} />
+			{/if}
+		{/each}
+	</div>
+	{#if root}
 		<select class="select select-bordered" on:mousedown|preventDefault={() => {}}
 			><option>Select Category</option>
 		</select>
-	</div>
-{:else}
-	<div class="join-item group/b inline-block relative">
-		<div
-			class="top-0 hidden group-hover/b:join join-vertical absolute left-full border border-base-200 outline-1 bg-base-100 divide-y divide-neutral"
-		>
-			{#each categories as c}
-				{#if c.children.length == 0}
-					<div class="join-item">{c.Name}</div>
-				{:else}
-					<svelte:self categories={c.children} name={c.Name} />
-				{/if}
-			{/each}
-		</div>
-		<div>{name}</div>
-	</div>
-{/if}
+	{:else}
+		<div class="join-item">{name}</div>
+	{/if}
+</div>
 
 <style lang="postcss">
 	.join-item {
